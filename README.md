820dd3e539204f382d5eeb5242605ebbdd5ed4b9
Shevids1996 <shevids@gmail.com>
2018-07-27 17:57:37 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+﻿# **Proxy Manager V1.0**
+![enter image description here](https://lh3.googleusercontent.com/_yjXu_pVZgAFpFeJbcvaya5PjwNvtJHniX8clmIlKtYhROVB_I0sGrJjBkUWQLQWaTI7aljdhVG0VQ)
+
+Proxy manager for python request sessions. Allows users to easily add proxies to sessions without the need to re-code logic for proxy management and use
+
+## Usage
+
+ - Install required module `pip install termcolor`
+ - Import into code using `from proxymanager import ProxyManager`
+ - Make call to method to get next proxy `proxy = ProxyManager().get_next_proxy()`
+ - assign proxy to session (`session.proxies = proxy`) 
+
+|Params|Use | Values |
+|--|--|--|
+| Random Proxy |Set to `True` to randomly pick a proxy from the list| Boolean |
+| Index  |Pass in a line number to recieve a specific proxy from the list | Integer |
+
+
+
+
+## Features
+
+ - [x] Preformatting proxys for use with requests 
+ - [x] Handling of unsupported formats
+ - [ ] Handling server assigned proxies (e.g Copped regional proxies)
+ - [ ] IP auth proxies 
+
+
+ 
+
---
+++ proxymanager.py
@@ -0,0 +1,65 @@
+#TODO: Fix this up to take multiple proxies, and format copped proxies to user:pass@ip:port
+from random import randint
+import datetime
+from termcolor import colored
+
+
+class ProxyManager:
+    def __init__(self):
+
+        self.formattedProxies  = []
+        self.badProxyArr       = []
+
+        with open('proxies.txt') as proxy_file:
+            self.proxies = proxy_file.readlines()
+
+            if len(self.proxies) == 0:
+                print(colored('[{}] :: No proxies in proxy file'.format(datetime.datetime.now().strftime(('%H:%M:%S'))),"red"))
+            else:
+                for proxies in range(len(self.proxies)):
+                    try:
+                        if self.proxies[proxies] != "":
+                            if "@" in self.proxies[proxies]:
+                                self.proxies.append(self.proxies[proxies])
+                            else:
+                                formatProxy = "{}:{}@{}:{}".format(self.proxies[proxies].rstrip().split(":")[2],self.proxies[proxies].rstrip().split(":")[3],self.proxies[proxies].rstrip().split(":")[0],self.proxies[proxies].rstrip().split(":")[1] )
+                                self.formattedProxies.append(formatProxy)
+                    except:
+                        if self.proxies[proxies] not in self.badProxyArr:
+                            self.badProxyArr.append(self.proxies[proxies])
+
+            badProxies = open("Bad_Proxies.txt", "w+")
+            for i in range(len(self.badProxyArr)):
+                badProxies.write("{}\r".format(self.badProxyArr[i]))
+
+
+        self.index = 0
+
+    def format(self, proxy):
+        return {
+            'http': 'http://{}'.format(proxy),
+            'https': 'https://{}'.format(proxy)
+        }
+
+
+    def get_next_proxy(self,randomProxy=True,index=0):
+        if self.index == len(self.formattedProxies):
+            return self.format(None)
+        if randomProxy:
+            rand = randint(0, len(self.formattedProxies) - 1)
+            print(colored('[{}] :: Added to session'.format(self.formattedProxies[rand]), "cyan",attrs=['bold']))
+            return self.format(self.formattedProxies[rand])
+        else:
+            print(colored('[{}] :: Added to session'.format(self.formattedProxies[index]), "cyan",attrs=['bold']))
+            return self.format(self.formattedProxies[index])
+
+i = 0
+
+while i < 100:
+    ProxyManager().get_next_proxy()
+    i = i + 1
+
+
+
+
+
