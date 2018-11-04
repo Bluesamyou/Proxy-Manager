cf19d2284b17c06e19df5070812852c8fab820c1
bluesamyou <shevids@gmail.com>
2018-11-04 22:53:49 +1100

    Added more proxy support

--- proxymanager.py
+++ proxymanager.py
@@ -1,29 +1,33 @@
-#TODO: Fix this up to take multiple proxies, and format copped proxies to user:pass@ip:port
 from random import randint
-import datetime
 from termcolor import colored
+import colorama
 
 
 class ProxyManager:
-    def __init__(self):
+    def __init__(self, type):
+
+        colorama.init()
 
         self.formattedProxies  = []
         self.badProxyArr       = []
 
-        with open('proxies.txt') as proxy_file:
+        with open(type) as proxy_file:
             self.proxies = proxy_file.readlines()
 
             if len(self.proxies) == 0:
-                print(colored('[{}] :: No proxies in proxy file'.format(datetime.datetime.now().strftime(('%H:%M:%S'))),"red"))
+                pass
             else:
                 for proxies in range(len(self.proxies)):
                     try:
                         if self.proxies[proxies] != "":
-                            if "@" in self.proxies[proxies]:
-                                self.proxies.append(self.proxies[proxies])
+                            if len(self.proxies[proxies].split(":"))== 2:
+                                self.formattedProxies.append(self.proxies[proxies].strip())
                             else:
-                                formatProxy = "{}:{}@{}:{}".format(self.proxies[proxies].rstrip().split(":")[2],self.proxies[proxies].rstrip().split(":")[3],self.proxies[proxies].rstrip().split(":")[0],self.proxies[proxies].rstrip().split(":")[1] )
-                                self.formattedProxies.append(formatProxy)
+                                if "@" in self.proxies[proxies]:
+                                    self.formattedProxies.append(self.proxies[proxies])
+                                else:
+                                    formatProxy = "{}:{}@{}:{}".format(self.proxies[proxies].rstrip().split(":")[2],self.proxies[proxies].rstrip().split(":")[3],self.proxies[proxies].rstrip().split(":")[0],self.proxies[proxies].rstrip().split(":")[1] )
+                                    self.formattedProxies.append(formatProxy)
                     except:
                         if self.proxies[proxies] not in self.badProxyArr:
                             self.badProxyArr.append(self.proxies[proxies])
@@ -36,10 +40,17 @@ class ProxyManager:
         self.index = 0
 
     def format(self, proxy):
-        return {
-            'http': 'http://{}'.format(proxy),
-            'https': 'https://{}'.format(proxy)
-        }
+        if proxy == None:
+            return {
+                'http': proxy,
+                'https': proxy
+            }
+        else:
+
+            return {
+                'http': 'http://{}'.format(proxy),
+                'https': 'https://{}'.format(proxy)
+            }
 
 
     def get_next_proxy(self,randomProxy=True,index=0):
@@ -52,14 +63,5 @@ class ProxyManager:
         else:
             print(colored('[{}] :: Added to session'.format(self.formattedProxies[index]), "cyan",attrs=['bold']))
             return self.format(self.formattedProxies[index])
-
-i = 0
-
-while i < 100:
-    ProxyManager().get_next_proxy()
-    i = i + 1
-
-
-
 
 
